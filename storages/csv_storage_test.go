d434fd5818766b395d7f3223244d59a186d75a18
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-03-25 17:18:47 +0300

    tests added to csv storage

---
+++ storages/csv_storage_test.go
@@ -0,0 +1,44 @@
+package storages
+
+import (
+	"os"
+	"path/filepath"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+const resourceDir = "W:/Golang/src/Backuper/resources"
+const resultDir = "W:/Golang/src/Backuper/result"
+const configName = "telegram.json"
+const testFile = "test_data.csv"
+
+func TestStoreRetrieve(t *testing.T) {
+
+	st := CsvStorage{}
+	test_data := strconv.FormatInt(time.Now().Unix(), 10)
+	test_slice := []string{test_data, test_data, test_data}
+
+	t.Log("\tStore record to .csv file")
+	{
+		file, err := os.OpenFile(filepath.Join(resultDir, testFile), os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+		require.NoError(t, err)
+		defer func() { _ = file.Close }()
+
+		err = st.Store(file, test_slice)
+		require.NoError(t, err)
+	}
+
+	t.Log("\tRead record from .csv file")
+	{
+		file, err := os.OpenFile(filepath.Join(resultDir, testFile), os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+		require.NoError(t, err)
+		defer func() { _ = file.Close }()
+
+		got, err := st.Retrieve(file, test_data)
+		require.NoError(t, err)
+		require.Contains(t, got, test_data)
+	}
+}
